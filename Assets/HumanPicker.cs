91fa1bff79f2e2c31fb9d717e9246ec9f01ca568
Atlas <AtlasTheTitan77@gmail.com>
2019-02-05 14:08:58 +0100

    CodeBoi
    the block placer, and human picker scripts

---
+++ Assets/HumanPicker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPicker : MonoBehaviour {
+
+	public GameObject human;
+	public GameObject skyDrag;
+	public LayerMask dragingMask;
+	private GameObject silo;
+	void Update(){
+		if(Input.GetMouseButton(0)){
+			RaycastHit hit;
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)){
+				if(human != null){
+					RaycastHit hit2;
+					Ray ray2 = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+					if(Physics.Raycast(ray2, out hit2, 50)){
+						human.transform.position = hit2.point;
+					}
+					if(Physics.Raycast(ray2, out hit2, 50, dragingMask)){
+						Debug.Log(hit2.transform.gameObject);
+						if(hit2.transform.tag == "Silo"){
+							silo = hit2.transform.gameObject;
+							silo.GetComponent<Renderer>().material.color = Color.red;
+						} else {
+							silo.GetComponent<Renderer>().material.color = Color.cyan;
+							silo = null;
+						}
+					}
+				} else {
+					if(hit.transform.tag == "Human"){
+						Debug.Log("that's a human");
+						human = hit.transform.gameObject;
+						human.GetComponent<Collider>().enabled = false;
+						human.GetComponent<Rigidbody>().useGravity = false;
+						skyDrag.SetActive(true);
+					}
+				}
+			}
+		}
+		if(Input.GetMouseButtonUp(0)){
+			if(human != null){
+				skyDrag.SetActive(false);
+				human.GetComponent<Rigidbody>().useGravity = true;
+				human.GetComponent<Collider>().enabled = true;
+				human = null;
+			}
+		}
+	}
+}
---
+++ Assets/PlaceBuildings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBuildings : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject ball;
+	private GameObject current;
+
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			Vector3 position = GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)){
+				current = Instantiate(current,transform.position,current.transform.rotation);
+				current.transform.position = hit.point;
+				current.transform.position = new Vector3(current.transform.position.x, current.transform.position.y + current.transform.localScale.y/2, current.transform.position.z);
+				Debug.Log("hit ground");
+			}
+
+			Debug.Log(position);
+		}
+	}
+
+	public void ChangeToBall(){
+		current = ball;
+	}
+
+	public void ChangeToBlock(){
+		current = block;
+	}
+}
