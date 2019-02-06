4d043cd4d75748a6f9f0e1c783cda1ae962c7605
Fonzie! <twistfons@live.nl>
2019-02-06 15:18:38 +0100

    Info overlay and mouse overlay

---
+++ Assets/Code/MouseOverlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverlay : MonoBehaviour
+{
+    public Vector3 offset;
+    private bool negative = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+    }
+
+    private void FollowMouse()
+    {
+        transform.position = Input.mousePosition + offset;
+    }
+
+    public void SetNegative(bool setting)
+    {
+        if (negative != setting)
+        {
+            negative = setting;
+
+            offset = new Vector3(offset.x, -offset.y, offset.z);
+
+            if (negative)
+                GetComponent<RectTransform>().pivot = new Vector2(GetComponent<RectTransform>().pivot.x, 1);
+            else
+                GetComponent<RectTransform>().pivot = new Vector2(GetComponent<RectTransform>().pivot.x, 0);
+        }
+    }
+}
