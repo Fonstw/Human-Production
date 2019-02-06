1a66b6b15a8e7952d583ddc85ff726f198e35dac
Atlas <AtlasTheTitan77@gmail.com>
2019-02-06 16:07:46 +0100

    Stupid effects

---
+++ Assets/Code/Electricity.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Electricity : MonoBehaviour
+{
+    public bool go = true;
+}
---
+++ Assets/Editor/GeneratorEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(Generator))]
+public class GeneratorEditor : Editor {
+
+	void OnSceneGUI(){
+		Generator fow = (Generator)target;
+		Handles.color = Color.black;
+		Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+	
+		Handles.color = Color.red;
+		foreach(Transform visibleTarget in fow.visibleTargets){
+			Handles.DrawLine(fow.transform.position, visibleTarget.position);
+		}
+	}
+}
