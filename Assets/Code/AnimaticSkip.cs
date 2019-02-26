0d1682bb4773d06987ab9138c05ac04cd09c7aaf
Jankymage <36413963+Jankymage@users.noreply.github.com>
2019-02-26 14:25:39 +0100

    Fatsoenlijk werkende animatic
    Fixed:
    Animatic prefab is nu daadwerkelijk een canvas, dat in iedere scene geplaatst kan worden.
    Script om door de animatic te skippen toegevoegd

---
+++ Assets/Code/AnimaticSkip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimaticSkip : MonoBehaviour
+{
+    
+    //het compoment met de playable director
+    public PlayableDirector playableDirector;
+
+    //lengte van animatie clips
+    public float clipTime = 5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //als spatie ingdrukt word skipt de player naar volgende clip in de animatic
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log(playableDirector.time);
+            playableDirector.time += clipTime - (playableDirector.time % clipTime);
+        }
+    }
+}
