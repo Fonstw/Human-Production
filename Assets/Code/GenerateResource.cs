24d45ffb67f4a68f36f48343d7664fd4955fc304
Atlas <AtlasTheTitan77@gmail.com>
2019-02-27 20:04:43 +0100

    Mines and upgrades

--- Assets/Code/GenerateResource.cs
+++ Assets/Code/GenerateResource.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class GenerateResource : MonoBehaviour
 {
+    public LayerMask groundTypes;
     public int resourceType;   // 1=computing, 2=power, 3=food
     public int generatesAmount;
     public float buildingTime;
     public string finishSound = "event:/FILEPATH";
 
     private ResourceManager gameManager;
+    private GroundTypes groundUnderneath;
     private string placeSound = "event:/object_build";
     private float ownTimer;
     private bool workDone = false;
@@ -29,16 +31,29 @@ public class GenerateResource : MonoBehaviour
         if (!workDone && Time.time > ownTimer)
             WorkIt();
 
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, 10, groundTypes)){
+            if(hit.transform.GetComponent<GroundType>()){
+                groundUnderneath = hit.transform.GetComponent<GroundType>().groundType;
+            }
+        }
     }
 
     private void WorkIt()
     {
-        if (resourceType == 1)   // power
+        if (resourceType == 1){   // power
             gameManager.AdjustCurrentPower(generatesAmount);
-        else if (resourceType == 2)   // food
+        }
+        else if (resourceType == 2) {// food
+            if(groundUnderneath != GroundTypes.Grass){
+                generatesAmount =  generatesAmount/2;
+            }
             gameManager.AdjustCurrentFood(generatesAmount);
-        else if (resourceType > 2)   // bioResearch
+            generatesAmount = generatesAmount*2;
+        }
+        else if (resourceType > 2){   // bioResearch
         gameManager.ChangeCurrentComputing(resourceType - 3, generatesAmount);
+        }
 
         workDone = true;
         FMODUnity.RuntimeManager.PlayOneShot(finishSound);
