bd4d33ad891eee1ff53955bba1484b0e42be0b65
Fonzie! <twistfons@live.nl>
2019-03-11 12:18:14 +0100

    Food and Mine erased from existence

--- Assets/Code/OverlayInfo.cs
+++ Assets/Code/OverlayInfo.cs
@@ -14,6 +14,8 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     [SerializeField] protected int food;
     [SerializeField] protected int computing;
 
+    private List<Color> initialColours;
+
     //public bool topHalfOfTheScreen;
 
     public float[] args;
@@ -21,7 +23,12 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialColours = new List<Color>();
+
+        Image[] images = overlay.GetComponentsInChildren<Image>();
+
+        foreach (Image image in images)
+            initialColours.Add(image.color);
     }
 
     // Update is called once per frame
@@ -42,27 +49,48 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             displayText = displayText.Replace("{"+r+"}", args[r].ToString());
 
         Text[] texts = overlay.GetComponentsInChildren<Text>();
+        Image[] images = overlay.GetComponentsInChildren<Image>();
 
         texts[0].text = displayText;
-        texts[1].text = time;
-        texts[2].text = power.ToString();
-        texts[3].text = food.ToString();
-        texts[4].text = computing.ToString();
 
-        if (power >= 0)
-            texts[2].color = new Color(.5f, 1, .5f);
-        else
-            texts[2].color = new Color(1, .5f, .5f);
+        if (time != "-1")
+        {
+            foreach (Text text in texts)
+                text.color = Color.white;
 
-        if (food >= 0)
-            texts[3].color = new Color(.5f, 1, .5f);
-        else
-            texts[3].color = new Color(1, .5f, .5f);
+            for (int i=0; i<images.Length; i++)
+                images[i].color = initialColours[i];
 
-        if (computing >= 0)
-            texts[4].color = new Color(.5f, 1, .5f);
-        else
-            texts[4].color = new Color(1, .5f, .5f);
+            texts[1].text = time;
+            texts[2].text = power.ToString();
+            texts[3].text = food.ToString();
+            texts[4].text = computing.ToString();
+
+            if (power >= 0)
+                texts[2].color = new Color(.5f, 1, .5f);
+            else
+                texts[2].color = new Color(1, .5f, .5f);
+
+            if (food >= 0)
+                texts[3].color = new Color(.5f, 1, .5f, 0);
+            else
+                texts[3].color = new Color(1, .5f, .5f, 0);
+
+            if (computing >= 0)
+                texts[4].color = new Color(.5f, 1, .5f);
+            else
+                texts[4].color = new Color(1, .5f, .5f);
+        }
+        else   // time == "-1"
+        {
+            foreach (Text text in texts)
+                text.color = new Color(0, 0, 0, 0);
+            texts[0].color = Color.white;
+
+            foreach (Image image in images)
+                image.color = new Color(0, 0, 0, 0);
+            images[0].color = initialColours[0];
+        }
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
