7efce4c1a9a7a2e977b205935b37da99d3376911
Fonzie! <twistfons@live.nl>
2019-03-12 21:29:46 +0100

    Info overlay works =D

--- Assets/Code/OverlayInfo.cs
+++ Assets/Code/OverlayInfo.cs
@@ -9,20 +9,17 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public GameObject overlay;
 
     public string titleText;
+    public Sprite[] icons;
     public string[] values;
     public string bodyText;
-
     public float[] args;
 
-    private List<Color> initialColours;
+    private int valueCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        initialColours = new List<Color>();
-
-        foreach (Text text in overlay.GetComponentsInChildren<Text>())
-            initialColours.Add(text.color);
+        valueCount = Mathf.Max(icons.Length, values.Length);
     }
 
     // Update is called once per frame
@@ -36,7 +33,8 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         // turn the overlay on
         overlay.SetActive(true);
         overlay.GetComponent<WindowBehaviour>().OpenWindow();
-        //overlay.GetComponent<MouseOverlay>().FollowMouse();
+        // avoid button bug
+        overlay.GetComponent<MouseOverlay>().ResetPosition();
 
         // parse <br> into actual working linebreaks (no, typing "\n" in the editor does not add a working linebreak)
         bodyText = bodyText.Replace("<br>", "\n");
@@ -45,20 +43,37 @@ public class OverlayInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         for (int r = 0; r < args.Length; r++)
             bodyText = bodyText.Replace("{"+r+"}", args[r].ToString());
 
-        Text[] texts = overlay.GetComponentsInChildren<Text>();
-        Image[] images = overlay.GetComponentsInChildren<Image>();
+        Image[] images = overlay.GetComponentsInChildren<Image>(true);
+        Text[] texts = overlay.GetComponentsInChildren<Text>(true);
 
         texts[0].text = titleText;
         texts[1].text = bodyText;
 
-        int id = 0;
+        for (int i = 1; i < 5; i++)
+            images[i].gameObject.SetActive(false);
+        for (int t = 2; t < 6; t++)
+            texts[t].gameObject.SetActive(false);
+
+        int id = 1;
+        foreach (Sprite i in icons)
+        {
+            images[id].gameObject.SetActive(true);
+            images[id].sprite = i;
+
+            id++;
+        }
+
+        id = 2;
         foreach (string v in values)
         {
-            overlay.GetComponentsInChildren<Text>()[id + 2].text = v;
+            texts[id].gameObject.SetActive(true);
+            texts[id].text = v;
 
-            if (v != "")
-                overlay.GetComponentsInChildren<Text>()[id + 2].color = initialColours[id];
+            id++;
         }
+        
+        // scale body text to available area
+        texts[1].rectTransform.sizeDelta = new Vector2(texts[1].rectTransform.sizeDelta.x, 20 + 40 * (4 - valueCount));
     }
 
     // when not hovering this thing
